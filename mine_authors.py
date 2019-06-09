939de5e8f747800ea19e4890cbd0697bfea095fd
Markus Borg <mail@mrksbrg.com>
2019-06-09 22:18:29 +0200

    More names, better lists

--- mine_authors.py
+++ mine_authors.py
@@ -5,15 +5,27 @@ Created on Fri Jun  7 23:57:13 2019
 @author: Markus Borg
 """
 
+from datetime import date
 import dblp
 
-rise_list = ["Markus Borg", "Ulrik Franke", "Ana Magazinius"]
+#rise_list = ["Markus Borg", "Ulrik Franke", "Ana Magazinius", "Joakim Fröberg", "Thomas Olsson", "Stefan Cedergren", "Jakob Axelsson"]
+#lu_list = ["Per Runeson", "Björn Regnell", "Martin Höst", "Elizabeth Bjarnason", "Emelie Engström"]
+bth_list = [ "Efi Papatheocharous"] #"Emil Alégroth", "Ali Nauman", "Fabian Fagerholm", "Javier Gonzalez Huerta", "Muhammad Usman"] #["Claes Wohlin", "Tony Gorschek", "Krzysztof Wnuk", "Michael Unterkalmsteiner", "Michael Mattsson", "Mikael Svahnberg", "Darja Smite", "Michael Felderer", "Jürgen Börstler"]
+chalmers_list = ["Jean-Philipp Steghöfer", "Christian Berger"] #"Robert Feldt", "Richard Torkar", "Ivica Crnkovic", "Richard Berntsson Svensson", "Francisco Gomes", "Gregory Gay", "Michel Chaudron", "Jan Bosch", "Eric Knauss", "Jennifer Horkoff","Eric Knauss", "Thorsten Berger", 
+kth_list = ["Benoit Baudry", "Martin Montperrus"] # "Frederic Loiret", "Karl Meinke"
+malmo_list = ["Helena Holmström Olsson", "Annabella Loconsole"]
+linkoping_list = ["Kristian Sandahl"]
+affiliation_list = bth_list
+
 sci_list = ["IEEE Trans. Software Eng.", "Empirical Software Engineering", "ACM Trans. Softw. Eng. Methodol.", "Autom. Softw. Eng.", "Information & Software Technology", "Requir. Eng.", " Software and System Modeling", "Software Quality Journal", "Journal of Systems and Software", "Journal of Software: Evolution and Process", "Softw. Test., Verif. Reliab.", "Softw., Pract. Exper.", "IET Software", "International Journal of Software Engineering and Knowledge Engineering"]
 
-f = open("output.txt","w+")
+x = date.today()
+print(x.day)
+#f = open(str(x) + "_RISE_output.txt","w+")
+f = open(str(x) + "_LU_output.txt","w+")
 i = 0
 
-for scholar in rise_list:
+for scholar in affiliation_list:
 	print(scholar)
 	authors = dblp.search(scholar)
 
@@ -37,17 +49,17 @@ for scholar in rise_list:
 	    co_authors = p.authors
 	    # count first-authorships
 	    if len(co_authors) > 0:
-		    if co_authors[0] == rise_list[i]:
+		    if co_authors[0] == affiliation_list[i]:
 		    	nbr_first_authorships += 1   
 	    counter += 1
-	    if counter>=500:
+	    if counter>=250:
 	    	break
 	
 	nbr_publications -= nbr_arxiv
 	seed_ratio = nbr_first_authorships / nbr_publications
 	quality_ratio = len(top_papers) / nbr_publications
 
-	result_string = rise_list[i] + " (" + str(nbr_publications) + " publ.) \t ### Seed ratio: " + str(seed_ratio) + " \t Quality ratio: " + str(quality_ratio) + "\n"
+	result_string = affiliation_list[i] + " (" + str(nbr_publications) + " publ.) \t ### Seed ratio: " + str(seed_ratio) + " \t Quality ratio: " + str(quality_ratio) + "\n"
 	print(result_string)
 
 	f.write(result_string) 
