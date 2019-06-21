1397759bee801157fc36fdda181f6f842c927dd4
Markus Borg <mail@mrksbrg.com>
2019-06-21 10:26:32 +0200

    Add ratio tests for Simon

--- test_all.py
+++ test_all.py
@@ -43,23 +43,28 @@ class TestClass:
         assert david.get_nbr_sci_publications() == 35        
         
         
-#    def test_simon_poulding(self):
-#        self.miner.process_group(self.test_scholar)
-#        self.scholars = self.miner.get_scholars()
-#        simon = self.scholars["Simon M. Poulding"]
-#
-#        # TC1: Test that DBLP returns a result
-#        assert self.scholars != None
-#        
-#        # TC2: Test that Simon Poulding has 48 DBLP entries
-#        assert simon.dblp_entries == 48
-#        
-#        # TC3: Test that the name is correctly processed
-#        assert simon.name == "Simon M. Poulding"
-#        
-#        # TC4: Test that Simon Poulding has 48 publications after removing duplicates
-#        assert simon.get_nbr_publications() == 44
-#        
-#        # TC5: Test that David Notkin has 35 publications in SCI-listed journals
-#        assert simon.get_nbr_sci_publications() == 8
+    def test_simon_poulding(self):
+        self.miner.process_group(self.test_scholar)
+        self.scholars = self.miner.get_scholars()
+        simon = self.scholars["Simon M. Poulding"]
+
+        # TC1: Test that DBLP returns a result
+        assert self.scholars != None
+        
+        # TC2: Test that Simon Poulding has 48 DBLP entries
+        assert simon.dblp_entries == 48
+        
+        # TC3: Test that the name is correctly processed
+        assert simon.name == "Simon M. Poulding"
+        
+        # TC4: Test that Simon Poulding has 48 publications after removing duplicates
+        assert simon.get_nbr_publications() == 44
+        
+        # TC5: Test that Simon Poulding has 8 publications in SCI-listed journals
+        assert simon.get_nbr_sci_publications() == 8
     
+        #TC6: Test that Simon Poulding has the correct ratios
+        simon.calc_stats()
+        assert simon._first_ratio == pytest.approx(0.364, 0.001)
+        assert simon._sci_ratio == pytest.approx(0.182, 0.001)
+        assert simon.get_nbr_sci_publications() == 8    
