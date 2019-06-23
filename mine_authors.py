994d4dbd361caed81682bdd3200926b1af895c01
Markus Borg <mail@mrksbrg.com>
2019-06-23 22:46:55 +0200

    Remove redundant code

--- mine_authors.py
+++ mine_authors.py
@@ -7,13 +7,13 @@ Created on Fri Jun  7 23:57:13 2019
 
 from scholar_miner import ScholarMiner
 
-fast_list = {"Annabella Loconsole": False, "Niklas Mellegård":False}
+fast_list = {"Annabella Loconsole":False}
 rise_list = {"Niklas Mellegård":False, "Efi Papatheocharous": False, "Mehrdad Saadatmand": False, "Pasqualina Potena": False, "Markus Borg": False, "Ulrik Franke": False,
 			  "Ana Magazinius": False, "Joakim Fröberg": False, "Thomas Olsson": False, "Stefan Cedergren": False, "Jakob Axelsson": False}
 lu_list = {"Per Runeson": False, "Björn Regnell": False, "Martin Höst": False, "Elizabeth Bjarnason": False, "Emelie Engström": False}
 bth_list = {"Claes Wohlin": False, "Tony Gorschek": False, "Krzysztof Wnuk": False, "Michael Unterkalmsteiner": False, "Michael Mattsson": False,
 			"Mikael Svahnberg": False, "Darja Smite": False, "Michael Felderer": False, "Jürgen Börstler": False, "Emil Alégroth": False, "Ali Nauman": False, "Fabian Fagerholm": False, "Javier Gonzalez Huerta": False, "Muhammad Usman": False}
-chalmers_list = {"Patrizio Pelliccione": False, "Riccardo Scandariato": False, "Miroslaw Staron": False, "Jan-Philipp Steghöfer": False, "Christian Berger": False, "Robert Feldt": False, "Richard Torkar": False, "Ivica Crnkovic": False,
+chalmers_list = {"Rogardt Heldal":False, "Kenneth Lind":False, "Patrizio Pelliccione": False, "Riccardo Scandariato": False, "Miroslaw Staron": False, "Jan-Philipp Steghöfer": False, "Christian Berger": False, "Robert Feldt": False, "Richard Torkar": False, "Ivica Crnkovic": False,
 				 "Richard Berntsson Svensson": False, "Francisco Gomes": False, "Gregory Gay": False, "Michel Chaudron": False, "Jan Bosch": False, "Jennifer Horkoff": False, 
 				 "Eric Knauss": False, "Thorsten Berger": False, "Gul Calikli": False, "Regina Hebig": False, "Philipp Leitner":False, "Agneta Nilsson":False}
 kth_list = {"Martin Monperrus": False, "Frederic Loiret": False, "Karl Meinke": False, "Benoit Baudry": False}
@@ -22,9 +22,11 @@ linkoping_list = {"Kristian Sandahl": False}
 mdh_list = {"Hans Hansson": False, "Jan Carlsson": False, "Antonio Cicchetti": False, "Federico Ciccozzi": False, "Séverine Sentilles": False,
 			"Kristina Lundqvist": False, "Daniel Sundmark": False, "Wasif Afzal": False, "Adnan Causevic": False, "Eduard Paul Enoiu": False, "Barbara Gallina":False}
 linne_list = {"Jesper Andersson": False}
+
 ericsson_list = {"Sigrid Eldh":False, "Kristian Wiklund":False, "Leif Jonsson":False, "Sahar Tahvili":False}
 others_list = {"":False}
 merged_lists = {**rise_list, **linne_list, **ericsson_list}
+all_lists = {**rise_list, **lu_list, **bth_list, **chalmers_list, **kth_list, **malmo_list, **linkoping_list, **mdh_list, **linne_list}
 #merged_lists = {**malmo_list, **linkoping_list}
 
 #sci_list = ["IEEE Trans. Software Eng.", "Empirical Software Engineering", "ACM Trans. Softw. Eng. Methodol.", "Autom. Softw. Eng.", "Information & Software Technology", "Requir. Eng.", " Software and System Modeling", "Software Quality Journal", "Journal of Systems and Software", "Journal of Software: Evolution and Process", "Softw. Test., Verif. Reliab.", "Softw., Pract. Exper.", "IET Software", "International Journal of Software Engineering and Knowledge Engineering"]
--- scholar_miner.py
+++ scholar_miner.py
@@ -16,12 +16,11 @@ sci_list = ["IEEE Trans. Software Eng.", "Empirical Software Engineering", "ACM 
 class ScholarMiner:
     
     def __init__(self):
-        self.output_file = open(str(date.today()) + "_output.txt","w+")
         self.scholars = {}
             
     def process_group(self, researchers):
         nbr_remaining = len(researchers)
-        while nbr_remaining > 0:
+        while nbr_remaining > 0: # an extra loop to tackle DBLP flakiness
             for scholar, processed in researchers.items():	
                 if not processed: # only proceed if the scholar hasn't been processed already
                     try:
@@ -33,46 +32,22 @@ class ScholarMiner:
                         time.sleep(5)
                         break
                                         
-        			# initiate variables
                     dblp_entries = len(search_res.publications)
-                    print("DBLP entries: ", dblp_entries)
-                    top_papers = []
-                    nbr_arxiv = 0
-                    nbr_first_authorships = 0
-                    nbr_first_top = 0
-                    total_text = ""
-                    
+                    print("DBLP entries: ", dblp_entries)          
                     current_scholar = SEScholar(scholar, dblp_entries)
                     self.scholars[scholar] = current_scholar
-        				
-                    #self.print_progress_bar(0, dblp_entries)
-                    
+        				                    
         			# traverse publications
                     i = 0
                     for p in search_res.publications:
                         self.print_progress_bar(i + 1, dblp_entries)
-
                         try:
-                            time.sleep(0.5) # There appears to be some race condition in the dblp package
-                            co_authors = p.authors
-        					
-                            # count SCI journals and how many as first author
-                            if len(co_authors) == 0: #skip papers with 0 authors
+                            time.sleep(0.5) # There appears to be some race condition in the dblp package  					
+                            if len(p.authors) == 0: #skip papers with 0 authors
                                     continue
                             elif p.type == "article":
                                 if p.journal == "CoRR": #skip ArXiv preprints
-                                    nbr_arxiv += 1
-                                    continue
-                                
-                                elif p.journal in sci_list:
-                                    top_papers.append(p.title)
-                                    if (co_authors[0] == scholar):
-                                        nbr_first_top += 1
-                                        total_text += "-" + p.title + "\n"                                                                       
-                                if len(co_authors) > 0:
-                                    if co_authors[0] == scholar:
-                                        nbr_first_authorships += 1
-                            
+                                    continue                          
                             current_publication = SEPublication(p.title, p.journal, p.year, p.authors)
                             current_scholar.add_publication(current_publication)
                             i += 1
@@ -80,25 +55,12 @@ class ScholarMiner:
                             print("ERROR. Processing one of the papers failed. Waiting...")
                             time.sleep(5)
                             break
-                        
-                    dblp_entries -= nbr_arxiv
-                    if dblp_entries > 0:
-                        seed_ratio = nbr_first_authorships / dblp_entries
-                        quality_ratio = len(top_papers) / dblp_entries
-                    else:
-                        seed_ratio = "N/A"
-                        quality_ratio = "N/A"
-                        
+                                                
                     current_scholar.calc_stats()
-                    result_string = scholar + " (" + str(dblp_entries) + " publ., First-in-top: " + str(nbr_first_top) + ") \t\t ### Self-made ratio: " + str(seed_ratio) + " \t Quality ratio: " + str(quality_ratio) + "\n"
-                    result_string += total_text
                     processed = True
                     nbr_remaining -= 1
-                    self.output_file.write(result_string)
-            
-        self.output_file.close()
     
-    # Print iterations progress
+    # Print progress bar for scholar processing
     def print_progress_bar(self, iteration, total):
         """
         Call in a loop to create terminal progress bar
@@ -121,7 +83,7 @@ class ScholarMiner:
         return self.scholars
     
     def write_scholars(self):
-        tmp = open(str(date.today()) + "_ATTEMPT.txt","w+")
+        tmp = open(str(date.today()) + "_SCHOLARS.txt","w+")
         for key, value in self.scholars.items():
             tmp.write(value.to_string() + "\n")
             tmp.write(value.sci_publications_to_string())
