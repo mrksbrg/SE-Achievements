80453bcde42cea318a2d2075dca5cef82588dbd8
Markus Borg <mail@mrksbrg.com>
2019-07-05 23:38:03 +0200

    LDA, NMF, WordCloud added

--- visualize_authors.py
+++ visualize_authors.py
@@ -14,6 +14,15 @@ from yellowbrick.text import TSNEVisualizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import text
+from nltk.stem.porter import PorterStemmer
+
+from wordcloud import WordCloud
+
+from sklearn.decomposition import LatentDirichletAllocation as LDA
+from sklearn.decomposition import NMF
+
+import matplotlib.pyplot as plt
+
 
 class Visualizer:
     def __init__(self, filename):
@@ -30,32 +39,72 @@ class Visualizer:
                 self.scholars_dict[row[0]] = row[1]
                 self.scholars_list.append(row[1])
                 line_count += 1
-            print(self.scholars_list)
+            #print(self.scholars_list)
+                
+    def display_topics(self, model, feature_names, no_top_words):
+        for topic_idx, topic in enumerate(model.components_):
+            print("Topic %d:" % (topic_idx))
+            print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
     
     def visualize(self):
-        vectorizer = CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS)
-        docs = vectorizer.fit_transform(self.scholars_list)
-        features = vectorizer.get_feature_names()
-        visualizer = FreqDistVisualizer(features=features)
-        visualizer.fit(docs)
-        visualizer.poof()
         
-        tfidf = TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS)
-        docs = tfidf.fit_transform(self.scholars_list)
-        labels = list(self.scholars_dict.keys())
-        tsne = TSNEVisualizer()
-        tsne.fit_transform(docs, labels)
-        tsne.poof()
+        # Preprocessing
+        extra_stop_words = text.ENGLISH_STOP_WORDS.union(["software", "engineering", "based", 
+                                                          "using", "case", "study", "oriented", 
+                                                          "driven", "workshop", "research",
+                                                          "übersetzerbau", "zur", "survey",
+                                                          "approach", "overview", "summary",
+                                                          "use", "multi", "experiment",
+                                                          "review", "non", "approaches",
+                                                          "controlled"])
+        tf_vec = CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(extra_stop_words))
+        tfidf_vec = TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(extra_stop_words))     
+        stemmer = PorterStemmer()
+        stemmed_corpus = []
+        for word in self.scholars_list:
+            stemmed_corpus.append(stemmer.stem(word))
+            
+        tf = tf_vec.fit_transform(stemmed_corpus)
+        tf_feature_names = tf_vec.get_feature_names()
+        tfidf = tfidf_vec.fit_transform(stemmed_corpus)
+        tfidf_feature_names = tfidf_vec.get_feature_names()
+        
+        nbr_topics = 8
+        nbr_words = 7
 
-#        tfidf  = TfidfVectorizer()
-#    
-#        docs = tfidf.fit_transform(corpus)
-#        labels = corpus.target
+        # LDA
+        lda = LDA(n_components=nbr_topics)  
+        lda.fit(tf)
+        
+        # NMF
+        nmf = NMF(n_components=nbr_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+
+        print("### LDA topics ###")
+        self.display_topics(lda, tf_feature_names, nbr_words)
+        print("\n### NMF topics ###")
+        self.display_topics(nmf, tfidf_feature_names, nbr_words)
+    
+        # t-SNE
+#        docs = vectorizer.fit_transform(self.scholars_list)
+#        features = vectorizer.get_feature_names()
+#        visualizer = FreqDistVisualizer(features=features)
+#        visualizer.fit(docs)
+#        visualizer.poof()      
 #        
-#        # Create the visualizer and draw the vectors
+#        docs = tfidf.fit_transform(stemmed_corpus)        
+#        labels = list(self.scholars_dict.keys())
 #        tsne = TSNEVisualizer()
-#        tsne.fit(docs, labels)
+#        tsne.fit_transform(tfidf, labels)
 #        tsne.poof()
+        
+        # Wordcloud
+#        wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+#        cloud = wordcloud.generate(stemmed_corpus[1])
+#
+#        plt.imshow(cloud, interpolation='bilinear')
+#        plt.title("Someone")
+#        plt.axis("off")
+#        plt.show()
  
 vis = Visualizer(str(date.today()) + "_Authors_all_titles.csv")
 vis.parse_csv()
