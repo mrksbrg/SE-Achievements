440f9227332a5a769fc5b7351a3208e02620d67b
Markus Borg <mail@mrksbrg.com>
2019-07-24 12:13:38 +0200

    First step toward templating with jinja2

--- scholar_tabulator.py
+++ scholar_tabulator.py
@@ -6,10 +6,16 @@ Created on Fri Jul  5 11:21:15 2019
 """
 
 import csv
+from jinja2 import Environment, FileSystemLoader
+
 
 class ScholarTabulator:
     def __init__(self, scholars):
-    	self.scholars = scholars
-    
-#	def write_table(self):
-#		Print("<Table>")
+        self.scholars = scholars
+
+    def write_table(self):
+
+        loader = FileSystemLoader('./index.html')
+        env = Environment(loader=loader)
+        #template = env.get_template('')
+        #print(template.render(date='2012-02-8', id='123', position='here', status='Waiting'))
