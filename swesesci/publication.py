53acc10d091b25c0be75e43b32e04b77369837b6
Markus Borg <markus.borg@ri.se>
2020-03-02 07:41:56 +0100

    Updated journal abbr.
    Some journals have new DBLP names

--- swesesci/publication.py
+++ swesesci/publication.py
@@ -1,6 +1,6 @@
 class SSSPublication:
     
-    sci_list = ["IEEE Trans. Software Eng.", "Empirical Software Engineering", "ACM Trans. Softw. Eng. Methodol.", "Autom. Softw. Eng.", "Information & Software Technology", "Requir. Eng.", " Software and System Modeling", "Software Quality Journal", "Journal of Systems and Software", "Journal of Software: Evolution and Process", "Journal of Software Maintenance", "Softw. Test., Verif. Reliab.", "Softw., Pract. Exper.", "IET Software", "International Journal of Software Engineering and Knowledge Engineering"]
+    sci_list = ["IEEE Trans. Software Eng.", "Empirical Software Engineering", "ACM Trans. Softw. Eng. Methodol.", "Autom. Softw. Eng.", "Inf. Softw. Technol.", "Requir. Eng.", "Software and System Modeling", "Software Quality Journal", "J. Syst. Softw.", "Journal of Software: Evolution and Process", "Journal of Software Maintenance", "Softw. Test., Verif. Reliab.", "Softw., Pract. Exper.", "IET Software", "International Journal of Software Engineering and Knowledge Engineering"]
     conf_list = ["ICSE", "FSE"]
 
     def __init__(self, title, journal, booktitle, year, authors):
