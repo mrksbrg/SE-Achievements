479ab66eeaabcb9d738b12af4c5b56955f996fc6
Markus Borg <markus.borg@ri.se>
2020-07-04 09:18:43 +0200

    Move non-ASCII check

--- swesesci/publication.py
+++ swesesci/publication.py
@@ -4,7 +4,7 @@ class SSSPublication:
     conf_list = ["ICSE", "FSE"]
 
     def __init__(self, title, journal, booktitle, year, authors):
-        self.title = self.ensure_ascii(title)
+        self.title = title
         self.journal = journal
         self.major_conf = False
         self.booktitle = booktitle
@@ -16,24 +16,6 @@ class SSSPublication:
         self.sci_listed = False
         if self.journal in self.sci_list:
             self.sci_listed = True
-
-    def ensure_ascii(self, string_to_check):
-        """
-        Replace any non-ASCII character with a whitespace (' ')
-        """
-        cleaned_string = str(string_to_check)
-        try:
-            string_to_check.encode('ascii')
-            cleaned_string = str(string_to_check)
-        except Exception:
-            print("Non-ASCII characters in the title: " + string_to_check)
-            for i in string_to_check:
-                try:
-                    i.encode('ascii')
-                except Exception:
-                    print("Non-ASCII character: " + i)
-                    cleaned_string = cleaned_string.replace(i, ' ')
-        return cleaned_string
 
     def __str__(self):
         if not self.journal is None:
--- swesesci/scholar_miner.py
+++ swesesci/scholar_miner.py
@@ -54,6 +54,9 @@ class ScholarMiner:
                         if len(p.authors) == 0:  # skip papers with 0 authors
                             continue
                         elif p.type == "article":
+                            # remove any non-ASCII characters, e.g., set theory
+                            p.title = self.ensure_ascii(p.title)
+
                             # Remove titles containing any of the substrings indicating editorial work
                             title_to_check = str(p.title).lower()
 
@@ -113,6 +116,24 @@ class ScholarMiner:
     def clear_all_scholars(self):
         for scholar in self.sss_scholars:
             scholar.clear()
+
+    def ensure_ascii(self, string_to_check):
+        """
+        Replace any non-ASCII character with a whitespace (' ')
+        """
+        cleaned_string = str(string_to_check)
+        try:
+            string_to_check.encode('ascii')
+            cleaned_string = str(string_to_check)
+        except Exception:
+            print("Non-ASCII characters in the title: " + string_to_check)
+            for i in string_to_check:
+                try:
+                    i.encode('ascii')
+                except Exception:
+                    print("Non-ASCII character: " + i)
+                    cleaned_string = cleaned_string.replace(i, ' ')
+        return cleaned_string
 
     def write_results(self):
         tmp = open(self.filename_prefix + "1_miner.txt","w+")
