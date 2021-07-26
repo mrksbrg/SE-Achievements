9ef81dad9b2500dccf731cb46980206fea583f67
Markus Borg <markus.borg@gmail.com>
2021-07-26 22:04:21 +0200

    Remove obsolete XML parsing

--- swesesci/scholar_miner_sax.py
+++ swesesci/scholar_miner_sax.py
@@ -10,15 +10,8 @@ import datetime
 import xml.sax
 import pandas as pd
 from collections import Counter
-import requests
-from lxml import etree
-from collections import namedtuple
-from sortedcontainers import SortedSet
-
-from . import publication
 from .publication import SSSPublication
 from .scholar_sax import SSSScholar
-
 
 class ScholarMiner(xml.sax.ContentHandler):
 
@@ -156,6 +149,12 @@ class ScholarMiner(xml.sax.ContentHandler):
             titles_per_affiliation.write(affiliation + ";" + titles + "\n")
         titles_per_author.close()
         titles_per_affiliation.close()
+
+    def get_scholars(self):
+        return self.sss_scholars
+
+    def get_coauthors(self):
+        return self.global_SSS_coauthors
 
     # SAX parsing
 
@@ -363,16 +362,6 @@ class ScholarMiner(xml.sax.ContentHandler):
         else:
             print("Done! No scholars were removed.")
 
-    def clear_all_scholars(self):
-        for scholar in self.sss_scholars:
-            scholar.clear()
-
-    def get_scholars(self):
-        return self.sss_scholars
-
-    def get_coauthors(self):
-        return self.coauthors
-
     # Print progress bar for scholar processing
     def print_progress_bar(self, iteration, total):
         """
@@ -400,194 +389,3 @@ class ScholarMiner(xml.sax.ContentHandler):
             if scholar.affiliation not in affiliations:
                 affiliations[scholar.affiliation] = ""
         return affiliations
-
-''' The DBLP query engine '''
-
-DBLP_BASE_URL = 'http://dblp.uni-trier.de/'
-DBLP_AUTHOR_SEARCH_URL = DBLP_BASE_URL + 'search/author'
-
-DBLP_PERSON_URL = DBLP_BASE_URL + 'pers/xk/{urlpt}'
-DBLP_PUBLICATION_URL = DBLP_BASE_URL + 'rec/bibtex/{key}.xml'
-
-class LazyAPIData(object):
-    def __init__(self, lazy_attrs):
-        self.lazy_attrs = set(lazy_attrs)
-        self.data = None
-
-    def __getattr__(self, key):
-        if key in self.lazy_attrs:
-            if self.data is None:
-                self.load_data()
-            return self.data[key]
-        raise AttributeError("Error when getting attribute: " + key)
-
-    def load_data(self):
-        pass
-
-class Author(LazyAPIData):
-    """
-    Represents a DBLP author. All data but the author's key is lazily loaded.
-    Fields that aren't provided by the underlying XML are None.
-
-    Attributes:
-    name - the author's primary name record
-    publications - a list of lazy-loaded Publications results by this author
-    homepages - a list of author homepage URLs
-    homonyms - a list of author aliases
-    """
-    def __init__(self, urlpt):
-        self.urlpt = urlpt
-        self.xml = None
-        super(Author, self).__init__(['name','publications','homepages',
-                                      'homonyms'])
-
-    def load_data(self):
-        resp = requests.get(DBLP_PERSON_URL.format(urlpt=self.urlpt))
-        # TODO error handling
-        xml = resp.content
-        self.xml = xml
-        root = etree.fromstring(xml)
-        data = {
-            'name':root.attrib['name'],
-            'publications':[Publication(k) for k in
-                            root.xpath('/dblpperson/dblpkey[not(@type)]/text()')],
-            'homepages':root.xpath(
-                '/dblpperson/dblpkey[@type="person record"]/text()'),
-            'homonyms':root.xpath('/dblpperson/homonym/text()')
-        }
-        self.data = data
-
-    def __str__(self):
-        return self.name + " - " + self.urlpt
-
-def first_or_none(seq):
-    try:
-        return next(iter(seq))
-    except StopIteration:
-        pass
-
-Publisher = namedtuple('Publisher', ['name', 'href'])
-Series = namedtuple('Series', ['text','href'])
-Citation = namedtuple('Citation', ['reference','label'])
-
-class Publication(LazyAPIData):
-    """
-    Represents a DBLP publication- eg, article, inproceedings, etc. All data but
-    the key is lazily loaded. Fields that aren't provided by the underlying XML
-    are None.
-
-    Attributes:
-    type - the publication type, eg "article", "inproceedings", "proceedings",
-    "incollection", "book", "phdthesis", "mastersthessis"
-    sub_type - further type information, if provided- eg, "encyclopedia entry",
-    "informal publication", "survey"
-    title - the title of the work
-    authors - a list of author names
-    journal - the journal the work was published in, if applicable
-    volume - the volume, if applicable
-    number - the number, if applicable
-    chapter - the chapter, if this work is part of a book or otherwise
-    applicable
-    pages - the page numbers of the work, if applicable
-    isbn - the ISBN for works that have them
-    ee - an ee URL
-    crossref - a crossrel relative URL
-    publisher - the publisher, returned as a (name, href) named tuple
-    citations - a list of (text, label) named tuples representing cited works
-    series - a (text, href) named tuple describing the containing series, if
-    applicable
-    """
-    def __init__(self, key):
-        self.key = key
-        self.xml = None
-        super(Publication, self).__init__( ['type', 'sub_type', 'mdate',
-                'authors', 'editors', 'title', 'year', 'month', 'journal',
-                'volume', 'number', 'chapter', 'pages', 'ee', 'isbn', 'url',
-                'booktitle', 'crossref', 'publisher', 'school', 'citations',
-                'series'])
-
-    def load_data(self):
-        resp = requests.get(DBLP_PUBLICATION_URL.format(key=self.key))
-        xml = resp.content
-        self.xml = xml
-        root = etree.fromstring(xml)
-        publication = first_or_none(root.xpath('/dblp/*[1]'))
-        if publication is None:
-            raise ValueError
-        data = {
-            'type':publication.tag,
-            'sub_type':publication.attrib.get('publtype', None),
-            'mdate':publication.attrib.get('mdate', None),
-            'authors':publication.xpath('author/text()'),
-            'editors':publication.xpath('editor/text()'),
-            'title':first_or_none(publication.xpath('title/text()')),
-            'year':int(first_or_none(publication.xpath('year/text()'))),
-            'month':first_or_none(publication.xpath('month/text()')),
-            'journal':first_or_none(publication.xpath('journal/text()')),
-            'volume':first_or_none(publication.xpath('volume/text()')),
-            'number':first_or_none(publication.xpath('number/text()')),
-            'chapter':first_or_none(publication.xpath('chapter/text()')),
-            'pages':first_or_none(publication.xpath('pages/text()')),
-            'ee':first_or_none(publication.xpath('ee/text()')),
-            'isbn':first_or_none(publication.xpath('isbn/text()')),
-            'url':first_or_none(publication.xpath('url/text()')),
-            'booktitle':first_or_none(publication.xpath('booktitle/text()')),
-            'crossref':first_or_none(publication.xpath('crossref/text()')),
-            'publisher':first_or_none(publication.xpath('publisher/text()')),
-            'school':first_or_none(publication.xpath('school/text()')),
-            'citations':[Citation(c.text, c.attrib.get('label',None))
-                         for c in publication.xpath('cite') if c.text != '...'],
-            'series':first_or_none(Series(s.text, s.attrib.get('href', None))
-                      for s in publication.xpath('series'))
-        }
-
-        self.data = data
-
-def search(author_str, running_number):
-    resp = requests.get(DBLP_AUTHOR_SEARCH_URL, params={'xauthor': author_str})
-    if running_number == -1:  # author without running number
-        #TODO error handling
-        root = etree.fromstring(resp.content)
-        result = [Author(urlpt) for urlpt in root.xpath('/authors/author/@urlpt')]
-        return result
-    else:  # author with running number
-        #TODO error handling
-
-        root = etree.fromstring(resp.content)
-        arr_of_authors = []
-        #counter = 0
-        #stopper = len(root.xpath('/authors/author/@urlpt'))
-        for urlpt in root.xpath('/authors/author/@urlpt'):
-            #print (counter)
-            #counter = counter + 1
-            #if counter == stopper:
-            #    print("Breaking!")
-            #    return arr_of_authors
-
-            resp1 = requests.get(DBLP_PERSON_URL.format(urlpt=urlpt))
-            xml = resp1.content
-            #print("before")
-            root1 = etree.fromstring(xml)
-            #print("after")
-            if root1.xpath('/dblpperson/homonym/text()'):
-                for hom_urlpt in root1.xpath('/dblpperson/homonym/text()'):
-                    arr_of_authors.append(Author(hom_urlpt))
-            else:
-                arr_of_authors.append(Author(urlpt))
-        return arr_of_authors
-
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in
-    this function because it is programmed to be pretty
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\n{}\n\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
