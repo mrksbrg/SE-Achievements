7bea48abac581b68dd4f96997c21d043395b10f1
Markus Borg <markus.borg@gmail.com>
2021-07-28 22:37:08 +0200

    Fix Mauro test

--- tests/test_4_nonasciititles.py
+++ tests/test_4_nonasciititles.py
@@ -20,38 +20,35 @@ class TestClass_NonASCIITitles:
         self.scholars = []
         self.affiliations = []
         self.filename_prefix = str(date.today()) + "_swese_"
-        self.test_nonascii_scholar = ["Mauro Caporuscio"]
+        self.test_nonascii_scholar = [("Mauro Caporuscio", "-1", "https://dblp.org/pid/c/MauroCaporuscio.xml")]
 
     def add_sss_scholars(self, process_list, affiliation):
-        for name in process_list:
-            words = name.split()
-            # check if author has a running number
-            if not words[len(words) - 1].isdigit():
-                self.scholars.append(SSSScholar(name, -1, affiliation))
-                tmp_aff = SSSAffiliation(affiliation)
-                if tmp_aff not in self.affiliations:
-                    tmp_aff.nbr_scholars += 1
-                    self.affiliations.append(tmp_aff)
-                else:
-                    curr = next((x for x in self.affiliations if affiliation == x.name), None)
-                    curr.nbr_scholars += 1
+        for person in process_list:
+            name = person[0]
+            running_number = person[1]
+            url = person[2]
+            # extract the pid from the url by substringing
+            try:
+                split1 = url.split("pid/")
+                split2 = split1[1].split(".xml")
+                pid = split2[0]
+            except IndexError:
+                print("Invalid format of input XML URL.")
+                return
+
+            self.scholars.append(SSSScholar(name, running_number, pid, url, affiliation, -1))
+            tmp_aff = SSSAffiliation(affiliation)
+            if tmp_aff not in self.affiliations:
+                tmp_aff.nbr_scholars += 1
+                self.affiliations.append(tmp_aff)
             else:
-                # author has a running number
-                tmp_scholar = SSSScholar(' '.join(map(str, words[0:len(words) - 1])), str(words[len(words) - 1]),
-                                         affiliation)
-                self.scholars.append(tmp_scholar)
-                tmp_aff = SSSAffiliation(affiliation)
-                if tmp_aff not in self.affiliations:
-                    tmp_aff.nbr_scholars += 1
-                    self.scholars.append(tmp_aff)
-                else:
-                    curr = next((x for x in self.affiliations if affiliation == x.name), None)
-                    curr.nbr_scholars += 1
+                curr = next((x for x in self.affiliations if affiliation == x.name), None)
+                curr.nbr_scholars += 1
 
     def test_mauro_caporuscio(self):
         self.add_sss_scholars(self.test_nonascii_scholar, "N/A")
         self.miner = ScholarMiner(self.filename_prefix, self.scholars, self.affiliations)
-        self.miner.process_group() # This involves dealing with non-ASCII characters
+        self.miner.parse_scholars() # This involves dealing with non-ASCII characters
         self.scholars = self.miner.get_scholars()
         mauro = None
         for scholar in self.scholars:
@@ -62,16 +59,16 @@ class TestClass_NonASCIITitles:
                 assert self.scholars != None
                 assert len(self.scholars) == 1
 
-                # TC2: Test that Thomas Olsson has at least 40 DBLP entries
+                # TC2: Test that Mauro has at least 40 DBLP entries
                 assert mauro.dblp_entries >= 40
 
                 # TC3: Test that the name is correctly processed
                 assert mauro.name == "Mauro Caporuscio"
 
-                # TC4: Test that Thomas Olsson has at least 30 publications after cleaning the list
+                # TC4: Test that Mauro has at least 30 publications after cleaning the list
                 assert mauro.nbr_publications >= 30
 
-                # TC5: Test that Thomas Olsson has non-zero ratios
+                # TC5: Test that Mauro has non-zero ratios
                 assert mauro.first_ratio >= 0.01
                 assert mauro.sci_ratio >= 0.01
                 assert mauro.nbr_sci_publications >= 1
