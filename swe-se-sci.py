60804fa1c8828b0f51cb54f146b3d02bf7a3fff1
Markus Borg <markus.borg@gmail.com>
2021-07-28 22:03:25 +0200

    Replace old stuff by new
    No more "sax" suffix

--- swe-se-sci.py
+++ swe-se-sci.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul  7 17:19:46 2019
+Created on Wed Jul 21 2021
 
 @author: Markus Borg
 """
@@ -8,94 +8,97 @@ Created on Sun Jul  7 17:19:46 2019
 import sys
 import os.path
 from datetime import date
+import xml.sax
 from swesesci.scholar import SSSScholar
 from swesesci.affiliation import SSSAffiliation
 from swesesci.scholar_miner import ScholarMiner
 from swesesci.scholar_analyzer import ScholarAnalyzer
 from swesesci.scholar_tabulator import ScholarTabulator
+#from swesesci.scholar_visualizer import ScholarVisualizer
 
 sss_scholars = []
 sss_affiliations = []
 
 def add_sss_scholars(process_list, affiliation):
-    for name in process_list:
-        words = name.split()
-        # check if author has a running number
-        if not words[len(words)-1].isdigit():
-            sss_scholars.append(SSSScholar(name, -1, affiliation))
-            tmp_aff = SSSAffiliation(affiliation)
-            if tmp_aff not in sss_affiliations:
-                tmp_aff.nbr_scholars += 1
-                sss_affiliations.append(tmp_aff)
-            else:
-                curr = next((x for x in sss_affiliations if affiliation == x.name), None)
-                curr.nbr_scholars += 1
+    for person in process_list:
+        name = person[0]
+        running_number = person[1]
+        url = person[2]
+        # extract the pid from the url by substringing
+        split1 = url.split("pid/")
+        split2 = split1[1].split(".xml")
+        pid = split2[0]
+
+        sss_scholars.append(SSSScholar(name, running_number, pid, url, affiliation, -1))
+        tmp_aff = SSSAffiliation(affiliation)
+        if tmp_aff not in sss_affiliations:
+            tmp_aff.nbr_scholars += 1
+            sss_affiliations.append(tmp_aff)
         else:
-            # author has a running number
-            tmp_scholar = SSSScholar(' '.join(map(str, words[0:len(words)-1])), str(words[len(words)-1]), affiliation)
-            sss_scholars.append(tmp_scholar)
-            tmp_aff = SSSAffiliation(affiliation)
-            if tmp_aff not in sss_affiliations:
-                tmp_aff.nbr_scholars += 1
-                sss_affiliations.append(tmp_aff)
-            else:
-                curr = next((x for x in sss_affiliations if affiliation == x.name), None)
-                curr.nbr_scholars += 1
+            curr = next((x for x in sss_affiliations if affiliation == x.name), None)
+            curr.nbr_scholars += 1
 
 # Swe-SE-SCI entry point
 if (len(sys.argv) == 1):
-    # fast_list = ["Stefan Cedergren", "Joakim Fröberg", "Thomas Olsson 0001"]
-    # add_sss_scholars(fast_list, "RISE")
-    # fast_list2 = ["Philipp Haller", "David Broman", "Paris Carbone", "Elena Troubitsyna", "Richard Glassey", "Musard Balliu"]
-    # add_sss_scholars(fast_list2, "MAU")
-    # bfast_list_3 = ["Danny Weyns"]
-    # add_sss_scholars(fast_list_3, "Linköping University")
-    # big_names = ["Lionel C. Briand", "Barry W. Boehm", "David Lorge Parnas", "Barbara A. Kitchenham", "Brad A. Myers", "Marlon Dumas", "Mark Harman"]
-    # add_sss_scholars(big_names, "Misc.")
-
-    rise_list = ["Niklas Mellegård", "Efi Papatheocharous", "Mehrdad Saadatmand", "Pasqualina Potena", "Markus Borg", "Ulrik Franke",
-                  "Ana Magazinius", "Jakob Axelsson", "Håkan Burden", "Peter Wallin", "Piotr Tomaszewski", "Thomas Olsson 0001"] #"Stig Larsson 0002",
+    test_list = [("Stefan Cedergren", "-1", "https://dblp.org/pid/116/6013.xml")]
+    #add_sss_scholars(test_list, "Fast test")
+    rise_list = [("Niklas Mellegård", "-1", "https://dblp.org/pid/30/8216.xml"),
+                 ("Efi Papatheocharous", "-1", "https://dblp.org/pid/76/302.xml"),
+                 ("Mehrdad Saadatmand", "-1", "https://dblp.org/pid/14/9944.xml"),
+                 ("Pasqualina Potena", "-1", "https://dblp.org/pid/49/2251.xml"),
+                 ("Markus Borg", "-1", "https://dblp.org/pid/47/9384.xml"),
+                 ("Ulrik Franke", "-1", "https://dblp.org/pid/71/3754.xml"),
+                 ("Ana Magazinius", "-1", "https://dblp.org/pid/46/8694.xml"),
+                 ("Jakob Axelsson", "-1", "https://dblp.org/pid/50/6797.xml"),
+                 ("Håkan Burden", "-1", "https://dblp.org/pid/02/10779.xml"),
+                 ("Peter Wallin", "-1", "https://dblp.org/pid/57/5749.xml"),
+                 ("Piotr Tomaszewski", "-1", "https://dblp.org/pid/75/5273.xml"),
+                 ("Thomas Olsson", "0001", "https://dblp.org/pid/31/5587-1.xml"),
+                 ("Stig Larsson", "0002", "https://dblp.org/pid/77/1925-2.xml")]
     add_sss_scholars(rise_list, "RISE Research Institutes of Sweden")
-    lu_list = ["Per Runeson", "Björn Regnell", "Martin Höst", "Elizabeth Bjarnason", "Emelie Engström", "Christin Lindholm", "Christoph Reichenbach", "Görel Hedin", "Martina Maggio"]
+    lu_list = [("Per Runeson", "-1", "https://dblp.org/pid/24/24.xml"),
+               ("Björn Regnell", "-1", "https://dblp.uni-trier.de/pid/09/1284.xml"),
+               ("Martin Höst", "-1", "https://dblp.uni-trier.de/pid/07/6594.xml"),
+               ("Elizabeth Bjarnason", "-1", "https://dblp.uni-trier.de/pid/19/1459.xml"),
+               ("Emelie Engström", "-1", "https://dblp.uni-trier.de/pid/31/2107.xml"),
+               ("Christin Lindholm", "-1", "https://dblp.uni-trier.de/pid/20/851.xml"),
+               ("Christoph Reichenbach", "-1", "https://dblp.uni-trier.de/pid/98/4527.xml"),
+               ("Görel Hedin", "-1", "https://dblp.uni-trier.de/pid/95/987.xml"),
+               ("Martina Maggio", "-1", "https://dblp.uni-trier.de/pid/02/8575.xml")]
     add_sss_scholars(lu_list, "Lund University")
-    bth_list = ["Claes Wohlin", "Tony Gorschek", "Krzysztof Wnuk", "Michael Unterkalmsteiner", "Michael Mattsson",
-                "Mikael Svahnberg", "Darja Smite", "Jürgen Börstler", "Emil Alégroth", "Ali Nauman", "Fabian Fagerholm", "Javier Gonzalez-Huerta", "Muhammad Usman",
-                "Davide Fucci", "Daniel Méndez Fernández", "Nauman Bin Ali", "Deepika Badampudi", "Emilia Mendes", "Ludwik Kuzniarz", "Lars Lundberg", "Niklas Lavesson"]
+    bth_list = [("Claes Wohlin", "-1", "https://dblp.org/pid/w/ClaesWohlin.xml"),
+                ("Tony Gorschek", "-1", "https://dblp.org/pid/82/3504.xml"),
+                ("Krzysztof Wnuk", "-1", "https://dblp.org/pid/86/2856.xml"),
+                ("Michael Unterkalmsteiner", "-1", "https://dblp.org/pid/08/8212.xml"),
+                ("Michael Mattsson", "-1", "https://dblp.org/pid/04/6269.xml"),
+                ("Mikael Svahnberg", "-1", "https://dblp.org/pid/81/5477.xml"),
+                ("Darja Smite", "-1", "https://dblp.org/pid/30/4138.xml"),
+                ("Jürgen Börstler", "-1", "https://dblp.org/pid/b/JBorstler.xml"),
+                ("Emil Alégroth", "-1", "https://dblp.org/pid/133/4686.xml"),
+                ("Nauman Bin Ali", "-1", "https://dblp.org/pid/37/10608.xml"),
+                ("Fabian Fagerholm", "-1", "https://dblp.org/pid/78/9972.xml"),
+                ("Javier Gonzalez-Huerta", "-1", "https://dblp.org/pid/98/8522.xml"),
+                ("Muhammad Usman", "0006", "https://dblp.org/pid/20/241-6.xml"),
+                ("Davide Fucci", "-1", "https://dblp.org/pid/70/9016.xml"),
+                ("Daniel Méndez Fernández", "-1", "https://dblp.org/pid/69/8522.xml"),
+                ("Deepika Badampudi", "-1", "https://dblp.org/pid/130/6487.xml"),
+                ("Emilia Mendes", "-1", "https://dblp.org/pid/m/EMendes.xml"),
+                ("Ludwik Kuzniarz", "-1", "https://dblp.org/pid/26/532.xml"),
+                ("Lars Lundberg", "-1", "https://dblp.org/pid/58/356.xml"),
+                ("Niklas Lavesson", "-1", "https://dblp.org/pid/55/1994.xml"),
+                ("Ahmad Nauman Ghazi", "-1", "https://dblp.org/pid/156/2314.xml")]
     add_sss_scholars(bth_list, "Blekinge Institute of Technology")
-    gbg_list = ["Rogardt Heldal", "Kenneth Lind", "Patrizio Pelliccione", "Miroslaw Staron", "Jan-Philipp Steghöfer", "Robert Feldt", "Richard Torkar", "Ivica Crnkovic",
-                     "Richard Berntsson-Svensson", "Francisco Gomes de Oliveira Neto", "Gregory Gay", "Jan Bosch", "Jennifer Horkoff", "John Hughes",
-                     "Eric Knauss", "Thorsten Berger", "Regina Hebig", "Agneta Nilsson", "Imed Hammouda", "Birgit Penzenstadler", "Lars Pareto",
-                     "Christian Berger", "Philipp Leitner"] #"Christian Berger 0001", "Philipp Leitner 0001" #defaulting to 0001 works
-    add_sss_scholars(gbg_list, "Chalmers / Gothenburg University")
-    kth_list = ["Martin Monperrus", "Frederic Loiret", "Karl Meinke", "Benoit Baudry", "Pontus Johnson", "Robert Lagerström", "Mathias Ekstedt", "Martin Törngren", "Hannes Holm", "Cyrille Artho", "Mira Kajko-Mattsson", "Philipp Haller", "David Broman", "Paris Carbone", "Elena Troubitsyna", "Richard Glassey", "Musard Balliu"]
-    add_sss_scholars(kth_list, "KTH Royal Institute of Technology")
-    su_list = ["Janis Stirna", "Jelena Zdravkovic"]
-    add_sss_scholars(su_list, "Stockholm University")
-    malmo_list = ["Helena Holmström Olsson", "Annabella Loconsole", "Patrik Berander", "Carl Magnus Olsson", "Jeanette Eriksson", "Bahtijar Vogel", "Hussan Munir"]
-    add_sss_scholars(malmo_list, "Malmö University")
-    linkoping_list = ["Kristian Sandahl", "Peter Fritzson", "Mariam Kamkar"]
-    add_sss_scholars(linkoping_list, "Linköping University")
-    mdh_list = ["Luciana Provenzano", "Alessio Bucaioni", "Hans A. Hansson", "Jan Carlsson", "Antonio Cicchetti", "Federico Ciccozzi", "Séverine Sentilles",
-                "Kristina Lundqvist", "Daniel Sundmark", "Wasif Afzal", "Adnan Causevic", "Eduard Paul Enoiu", "Barbara Gallina", "Mikael Sjödin", "Daniel Flemström", "Saad Mubeen"]
+    chalmers_list = [("Philipp Leitner", "0001", "https://dblp.org/pid/03/5268.xml")]
+    add_sss_scholars(chalmers_list, "Chalmers / Gothenburg University")
+    mdh_list = [("Alessio Bucaioni", "-1", "https://dblp.org/pid/143/4024.xml")]
     add_sss_scholars(mdh_list, "Mälardalen University")
-    linne_list = ["Jesper Andersson", "Morgan Ericsson", "Narges Khakpour", "Danny Weyns", "Welf Löwe", "Francesco Flammini", "Francis Palma", "Andreas Kerren", "Rafael Messias Martins", "Anna Wingkvist", "Jonas Lundberg", "Mauro Caporuscio"]
-    add_sss_scholars(linne_list, "Linneaus Univerity")
-    skovde_list = ["Björn Lundell", "Sten Andler", "Birgitta Lindström", "Henrik Gustavsson", "Jonas Gamalielsson",
-                   "Simon Butler", "Joeri van Laere", "Anne Persson", "Beatrice Alenljung", "Jeremy Rose"]
-    add_sss_scholars(skovde_list, "Skövde University")
-    karlstad_list = ["Sebastian Herold", "Bestoun S. Ahmed", "Martin Blom", "Muhammad Ovais Ahmad"]
-    add_sss_scholars(karlstad_list, "Karlstad University")
-    jonkoping_list = ["Anders Adlemo"]
-    add_sss_scholars(jonkoping_list, "Jönköping University")
-    orebro_list = ["Panagiota Chatzipetrou"]
-    add_sss_scholars(orebro_list, "Örebro University")
-    halmstad_university = ["Walid Taha"]
-    add_sss_scholars(halmstad_university, "Halmstad University")
 
 # Process scholar provided in the argument
 else:
+    specific_scholar = (sys.argv[1], sys.argv[2], sys.argv[3])
     custom_list = []
-    custom_list.append(sys.argv[1])
+    custom_list.append(specific_scholar)
+
     add_sss_scholars(custom_list, "N/A")
 
 # Prepare the process
@@ -108,8 +111,11 @@ filename_prefix = os.path.join(subdirectory, str(date.today()) + "_sss_")
 
 # 1. Mine the scholars, write the results
 print("####### Step 1 - Mining scholars #######")
+parser = xml.sax.make_parser()
+# turn off namespaces
+parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 miner = ScholarMiner(filename_prefix, sss_scholars, sss_affiliations)
-miner.process_group()
+miner.parse_scholars()
 miner.write_results()
 sss_scholars = miner.get_scholars()
 
