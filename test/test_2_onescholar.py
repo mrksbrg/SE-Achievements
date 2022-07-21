606b59c8d9ad7893ba6c73e501b7fcad51157ae0
Markus Borg <markus.borg@ri.se>
2022-07-21 10:57:19 +0200

    Refactor test structure

--- test/test_2_onescholar.py
+++ test/test_2_onescholar.py
@@ -7,6 +7,7 @@ Created on Sat Jun 15 16:50:30 2019
 
 import pytest
 import os.path
+import csv
 from datetime import date
 from swesesci.scholar import SSSScholar
 from swesesci.affiliation import SSSAffiliation
@@ -14,55 +15,69 @@ from swesesci.scholar_miner import ScholarMiner
 from swesesci.scholar_analyzer import ScholarAnalyzer
 from swesesci.scholar_tabulator import ScholarTabulator
 
+def string_splitter(scholar_string):
+    affiliation = scholar_string[0]
+    name = scholar_string[1]
+    running_number = scholar_string[2]
+    url = scholar_string[3]
+
+    try:
+        split1 = url.split("pid/")
+        split2 = split1[1].split(".xml")
+        pid = split2[0]
+    except IndexError:
+        print("Invalid format of input XML URL. (" + name + ")")
+        raise IndexError
+
+    return affiliation, name, running_number, pid, url
+
 class TestClass_OneScholar:
 
     def setup_method(self):
-        self.scholars = []
-        self.affiliations = []
+        self.sss_scholars = []
+        self.sss_affiliations = []
         subdirectory = "output"
         try:
             os.mkdir(subdirectory)
         except Exception:
             pass
         self.filename_prefix = os.path.join(subdirectory, str(date.today()) + "_sss_")
-        self.test_scholar = [("David Notkin", "-1", "https://dblp.org/pid/n/DavidNotkin.xml")]
 
-    def add_sss_scholars(self, process_list, affiliation):
-        for person in process_list:
-            name = person[0]
-            running_number = person[1]
-            url = person[2]
-            # extract the pid from the url by substringing
-            try:
-                split1 = url.split("pid/")
-                split2 = split1[1].split(".xml")
-                pid = split2[0]
-            except IndexError:
-                print("Invalid format of input XML URL.")
-                return
+    def get_affiliation_list(self, scholars):
+        tmp_list = []
+        for scholar in self.sss_scholars:
+            tmp_aff = SSSAffiliation(scholar.affiliation)
+            existing_aff = tmp_aff in self.sss_affiliations
 
-            self.scholars.append(SSSScholar(name, running_number, pid, url, affiliation, -1))
-            tmp_aff = SSSAffiliation(affiliation)
-            if tmp_aff not in self.affiliations:
+            if existing_aff is False:
                 tmp_aff.nbr_scholars += 1
-                self.affiliations.append(tmp_aff)
+                tmp_list.append(tmp_aff)
             else:
-                curr = next((x for x in self.affiliations if affiliation == x.name), None)
-                curr.nbr_scholars += 1
+                existing_aff.nbr_scholars += 1
+
+        return tmp_list
 
     def test_david_notkin(self):
-        self.add_sss_scholars(self.test_scholar, "N/A")
-        self.miner = ScholarMiner(self.filename_prefix, self.scholars, self.affiliations)
+        with open('test/test_2_onescholar.csv', newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            candidate_scholars = list(reader)
+
+        scholar_list = list(map(string_splitter, candidate_scholars))
+        [self.sss_scholars.append(SSSScholar(x[1], x[2], x[3], x[4], x[0], -1)) for x in scholar_list]
+        self.sss_affiliations = self.get_affiliation_list(self.sss_scholars)
+
+        self.miner = ScholarMiner(self.filename_prefix, self.sss_scholars, self.sss_affiliations)
         self.miner.parse_scholars()
-        self.scholars = self.miner.get_scholars()
+        self.sss_scholars = self.miner.get_scholars()
+
         david = None
-        for scholar in self.scholars:
+        for scholar in self.sss_scholars:
             if scholar.name == "David Notkin":
                 david = scholar
 
         # TC1: Test that DBLP returns a result
-        assert self.scholars != None
-        assert len(self.scholars) == 1
+        assert self.sss_scholars != None
+        assert len(self.sss_scholars) == 1
 
         # TC2: Test that David Notkin has 159 DBLP entries
         assert david.dblp_entries == 159
@@ -92,11 +107,11 @@ class TestClass_OneScholar:
         assert file_stats_csv.st_size == pytest.approx(67, 1)
 
         # TC8: Test analyzer
-        analyzer = ScholarAnalyzer(self.filename_prefix, self.scholars, self.affiliations)
+        analyzer = ScholarAnalyzer(self.filename_prefix, self.sss_scholars, self.sss_affiliations)
         analyzer.analyze_individual_research_interests()
         assert david.sss_contrib == 5.94
         assert david.sss_rating == 21.82
 
         # TC10: Test tabulator
-        tabulator = ScholarTabulator(self.filename_prefix, self.scholars, self.affiliations)
+        tabulator = ScholarTabulator(self.filename_prefix, self.sss_scholars, self.sss_affiliations)
         tabulator.write_tables()
