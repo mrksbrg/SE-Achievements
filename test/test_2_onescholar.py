3e6f3141a8551e0a24cc0cc6035cfdc738803de8
Markus Borg <markus.borg@gmail.com>
2022-07-24 19:09:39 +0200

    Remove obsolete imports TC2
    Issue #17

--- test/test_2_onescholar.py
+++ test/test_2_onescholar.py
@@ -7,10 +7,7 @@ Created on Sat Jun 15 16:50:30 2019
 
 import pytest
 import os.path
-import csv
 from datetime import date
-from swesesci.scholar import SSSScholar
-from swesesci.affiliation import SSSAffiliation
 from swesesci.scholar_reader import ScholarReader
 from swesesci.scholar_miner import ScholarMiner
 from swesesci.scholar_analyzer import ScholarAnalyzer
